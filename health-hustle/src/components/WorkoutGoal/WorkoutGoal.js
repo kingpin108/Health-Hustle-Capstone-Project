0f38a9610e16eb6ea44c6bb9879bd12533a41a9e
Vishal Gala <118876257+kingpin108@users.noreply.github.com>
2023-06-22 21:26:53 -0400

    Props Error Solved

--- health-hustle/src/components/WorkoutGoal/WorkoutGoal.js
+++ health-hustle/src/components/WorkoutGoal/WorkoutGoal.js
@@ -3,185 +3,186 @@ import { StyleSheet, ImageBackground, View, Dimensions, SafeAreaView, Image, Scr
 import { Drawer, Appbar, Divider, Text, Button, Checkbox, TextInput, Switch, RadioButton, SegmentedButtons, Card, Title, Paragraph } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import ProgressCircle from 'react-native-progress-circle'
+// import ProgressCircle from 'react-native-progress-circle'
 import styles from './styles';
 import { AuthContext } from '../../contexts/AuthContext';
 import { database } from '../../database/config';
 
+
 const WorkoutGoal = () => {
-  const navigation = useNavigation();
-  const { user } = useContext(AuthContext);
-  const { uid } = user;
+    const navigation = useNavigation();
 
-  const handleHomePress = () => {
-    navigation.navigate('Workout');
-  };
+    const handleHomePress = () => {
+        navigation.navigate('Workout');
+    };
 
-  const handleBack = () => {
-    navigation.goBack();
-  };
+    const handleBack = () => {
+        navigation.goBack();
+    };
 
-  const [value, setValue] = useState('A');
-  const [duration, setDuration] = useState('');
-  const [count, setCount] = useState('');
-  const [cardData, setCardData] = useState([]);
-  const [percent, setPercent] = useState(20);
+    const [value, setValue] = useState('A');
+    const [duration, setDuration] = useState('');
+    const [count, setCount] = useState('');
+    const [cardData, setCardData] = useState([]);
+    const [percent, setPercent] = useState(20);
 
-  const handleSubmit = () => {
-    if (value === 'A' && duration) {
-      const newCard = { title: 'Duration', description: duration + ' minutes' };
-      setCardData(prevData => [...prevData, newCard]);
-      setDuration('');
+    const handleSubmit = () => {
+        if (value === 'A' && duration) {
+            const newCard = { title: 'Duration', description: duration + ' minutes' };
+            setCardData(prevData => [...prevData, newCard]);
+            setDuration('');
 
-      const formDataRef = database.ref(`users/${uid}/goals`);
-      formDataRef.push({
-        type: 'Duration',
-        value: duration,
-        text: 'minutes',
-        percent: percent,
-      });
-    } else if (value === 'B' && count) {
-      const newCard = { title: 'Step Count', description: count };
-      setCardData(prevData => [...prevData, newCard]);
-      setCount('');
+            const formDataRef = database.ref(`users/${uid}/goals`);
+            formDataRef.push({
+                type: 'Duration',
+                value: duration,
+                text: 'minutes',
+                percent: percent,
+            });
+        } else if (value === 'B' && count) {
+            const newCard = { title: 'Step Count', description: count };
+            setCardData(prevData => [...prevData, newCard]);
+            setCount('');
 
-      const formDataRef = database.ref(`users/${uid}/goals`);
-      formDataRef.push({
-        type: 'Step Count',
-        value: count,
-        text: 'steps',
-        percent: percent,
-      });
-    }
-  };
+            const formDataRef = database.ref(`users/${uid}/goals`);
+            formDataRef.push({
+                type: 'Step Count',
+                value: count,
+                text: 'steps',
+                percent: percent,
+            });
+        }
+    };
 
-  useEffect(() => {
-    const fetchGoals = async () => {
-      try {
+    useEffect(() => {
         const goalsRef = database.ref(`users/${uid}/goals`);
-        const snapshot = await goalsRef.once('value');
-        const goalsData = snapshot.val();
-        const goalsArray = Object.values(goalsData);
-        setCardData(goalsArray);
-      } catch (error) {
-        console.error('Error fetching goals:', error);
-      }
-    };
 
-    fetchGoals();
+        const fetchGoals = async () => {
+            try {
+                const snapshot = await goalsRef.once('value');
+                const goalsData = snapshot.val();
+                const goalsArray = Object.values(goalsData);
+                setCardData(goalsArray);
+            } catch (error) {
+                console.error('Error fetching goals:', error);
+            }
+        };
 
-    return () => {
-      // Cleanup logic if needed
-    };
-  }, [uid]);
+        fetchGoals();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
+        return () => goalsRef.off();
+    }, [uid]);
+
+    const { user } = useContext(AuthContext);
+    const { uid } = user;
+
+    useEffect(() => {
         const formDataRef = database.ref(`users/${uid}/formData`);
-        const snapshot = await formDataRef.once('value');
-        const formData = snapshot.val();
-        // setPercent(formData.workoutDuration)
-      } catch (error) {
-        console.error('Error fetching formData:', error);
-      }
-    };
 
-    fetchData();
+        const fetchData = async () => {
+            try {
+                const snapshot = await formDataRef.once('value');
+                const formData = snapshot.val();
+                setPercent(formData.workoutDuration)
+            } catch (error) {
+                console.error('Error fetching formData:', error);
+            }
+        };
 
-    return () => formDataRef.off();
-  }, [uid]);
+        fetchData();
 
+        return () => formDataRef.off();
+    }, [uid]);
 
-  return (
-    <>
-      <StatusBar bar-style='dark-content' />
-      <Appbar.Header style={styles.appHeaderContainer}>
-        <Appbar.BackAction onPress={handleBack} />
-        <Appbar.Content
-          title="Set Workout Goal"
-          titleStyle={styles.appHeaderTitle}
-        />
-      </Appbar.Header>
-      <SafeAreaView style={styles.safeAreaContainer}>
-        <View style={styles.container}>
-          <SegmentedButtons
-            value={value}
-            onValueChange={setValue}
-            buttons={[
-              {
-                value: 'A',
-                label: 'Workout Duration',
-              },
-              {
-                value: 'B',
-                label: 'Step Count',
-              },
-            ]}
-          />
+    return (
+        <>
+            <StatusBar bar-style='dark-content' />
+            <Appbar.Header style={styles.appHeaderContainer}>
+                <Appbar.BackAction onPress={handleBack} />
+                <Appbar.Content
+                    title="Set Workout Goal"
+                    titleStyle={styles.appHeaderTitle}
+                />
+            </Appbar.Header>
+            <SafeAreaView style={styles.safeAreaContainer}>
+                <View style={styles.container}>
+                    <SegmentedButtons
+                        value={value}
+                        onValueChange={setValue}
+                        buttons={[
+                            {
+                                value: 'A',
+                                label: 'Workout Duration',
+                            },
+                            {
+                                value: 'B',
+                                label: 'Step Count',
+                            },
+                        ]}
+                    />
 
-          {value === 'A' && (
-            <View style={{ borderBottomColor: 'grey', borderBottomWidth: 2, alignItems: 'center', width: 350 }}>
-              <Text variant='titleLarge' style={{ paddingVertical: 10 }}>Enter target time in minutes</Text>
-              <TextInput
-                label="Enter Duration"
-                value={duration}
-                onChangeText={text => setDuration(text)}
-                keyboardType='numeric'
-                style={{ marginVertical: 10, width: 300 }}
-              />
-              <Button mode="contained" onPress={handleSubmit} style={{ marginVertical: 10 }}>
-                Add Goal
-              </Button>
-            </View>
-          )}
+                    {value === 'A' && (
+                        <View style={{ borderBottomColor: 'grey', borderBottomWidth: 2, alignItems: 'center', width: 350 }}>
+                            <Text variant='titleLarge' style={{ paddingVertical: 10 }}>Enter target time in minutes</Text>
+                            <TextInput
+                                label="Enter Duration"
+                                value={duration}
+                                onChangeText={text => setDuration(text)}
+                                keyboardType='numeric'
+                                style={{ marginVertical: 10, width: 300 }}
+                            />
+                            <Button mode="contained" onPress={handleSubmit} style={{ marginVertical: 10 }}>
+                                Add Goal
+                            </Button>
+                        </View>
+                    )}
 
-          {value === 'B' && (
-            <View style={{ borderBottomColor: 'grey', borderBottomWidth: 2, alignItems: 'center', width: 350 }}>
-              <Text variant='titleLarge' style={{ paddingVertical: 10 }}>Enter step target in minutes</Text>
-              <TextInput
-                label="Enter Count"
-                value={count}
-                onChangeText={text => setCount(text)}
-                keyboardType='numeric'
-                style={{ marginVertical: 10, width: 300 }}
-              />
-              <Button mode="contained" onPress={handleSubmit} style={{ marginVertical: 10 }}>
-                Add Goal
-              </Button>
-            </View>
-          )}
+                    {value === 'B' && (
+                        <View style={{ borderBottomColor: 'grey', borderBottomWidth: 2, alignItems: 'center', width: 350 }}>
+                            <Text variant='titleLarge' style={{ paddingVertical: 10 }}>Enter step target in minutes</Text>
+                            <TextInput
+                                label="Enter Count"
+                                value={count}
+                                onChangeText={text => setCount(text)}
+                                keyboardType='numeric'
+                                style={{ marginVertical: 10, width: 300 }}
+                            />
+                            <Button mode="contained" onPress={handleSubmit} style={{ marginVertical: 10 }}>
+                                Add Goal
+                            </Button>
+                        </View>
+                    )}
 
-          <ScrollView contentContainerStyle={{ padding: 16 }} style={{ margin: 10 }}>
-            {cardData.length === 0 ? (
-              <Text style={styles.noGoalsText}>No Goals set</Text>
-            ) : (
-              cardData.map((item, index) => (
-                <Card key={index} style={styles.card}>
-                  <Card.Content style={styles.cardContent}>
-                    <View style={styles.cardTextContainer}>
-                      <Title>{item.type}</Title>
-                      <Paragraph>{item.value} {item.text}</Paragraph>
-                    </View>
-                    <ProgressCircle
-                      percent={item.percent}
-                      radius={30}
-                      borderWidth={4}
-                      color="purple"
-                      shadowColor="#999"
-                      bgColor="#fff"
-                    >
-                      <Text style={{ fontSize: 14 }}>{item.percent + '%'}</Text>
-                    </ProgressCircle>
-                  </Card.Content>
-                </Card>
-              ))
-            )}
-          </ScrollView>
-        </View>
-      </SafeAreaView>
-    </>
-  );
+                    <ScrollView contentContainerStyle={{ padding: 16 }} style={{ margin: 10 }}>
+                        {cardData.length === 0 ? (
+                            <Text style={styles.noGoalsText}>No Goals set</Text>
+                        ) : (
+                            cardData.map((item, index) => (
+                                <Card key={index} style={styles.card}>
+                                    <Card.Content style={styles.cardContent}>
+                                        <View style={styles.cardTextContainer}>
+                                            <Title>{item.type}</Title>
+                                            <Paragraph>{item.value}</Paragraph>
+                                        </View>
+                                        {/* <ProgressCircle
+                                            percent={0}
+                                            radius={30}
+                                            borderWidth={4}
+                                            color="purple"
+                                            shadowColor="#999"
+                                            bgColor="#fff"
+                                        >
+                                            <Text style={{ fontSize: 14 }}>{0 + '%'}</Text>
+                                        </ProgressCircle> */}
+                                    </Card.Content>
+                                </Card>
+                            ))
+                        )}
+                    </ScrollView>
+                </View>
+            </SafeAreaView>
+        </>
+    );
 };
 
 export default WorkoutGoal;
