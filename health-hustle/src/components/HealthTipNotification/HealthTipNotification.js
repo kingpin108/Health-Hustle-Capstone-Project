cf3b90d2f0930ee77aaa54430e3c3f56e8a13a9d
RuhiDosani <118700888+RuhiDosani@users.noreply.github.com>
2023-07-31 01:16:48 -0400

    Health tip notification modified

--- health-hustle/src/components/HealthTipNotification/HealthTipNotification.js
+++ health-hustle/src/components/HealthTipNotification/HealthTipNotification.js
@@ -170,7 +170,8 @@ export default function HealthTipNotification() {
     }, [uid]);
 
     const themeStyles = theme ? darkThemeStyles : lightThemeStyles;
-
+ 
+   
 
     const paperTheme =
         theme
@@ -257,7 +258,42 @@ export default function HealthTipNotification() {
 };
 
 async function scheduleReminder(notificationTime) {
+
+    const tipArray = [
+        "Stay hydrated by drinking enough water throughout the day.",
+        "Eat a balanced diet with plenty of fruits and vegetables.",
+        "Get regular exercise to keep your body and mind healthy.",
+        "Practice good hygiene by washing your hands frequently.",
+        "Take breaks and stretch if you spend long hours sitting or working at a desk.",
+        "Get enough sleep to support your overall well-being.",
+        "Limit processed foods and choose whole, unprocessed foods whenever possible.",
+        "Practice portion control to maintain a healthy weight.",
+        "Reduce your intake of sugary drinks and opt for water or herbal tea instead.",
+        "Manage stress through relaxation techniques like deep breathing or meditation.",
+        "Protect your skin from the sun by wearing sunscreen and protective clothing.",
+        "Engage in activities that promote mental well-being, such as reading or hobbies.",
+        "Avoid smoking and limit alcohol consumption for better overall health.",
+        "Incorporate strength training exercises to maintain muscle mass and bone health.",
+        "Practice good posture to prevent back and neck pain.",
+        "Schedule regular check-ups and screenings to catch any potential health issues early.",
+        "Limit screen time and take regular breaks to protect your eyes.",
+        "Practice mindfulness and engage in activities that bring you joy.",
+        "Maintain a positive social support network for emotional well-being.",
+        "Limit processed and fried foods, and opt for healthier cooking methods like baking or grilling.",
+        "Incorporate aerobic exercises like brisk walking or cycling into your routine.",
+        "Practice deep breathing exercises to reduce stress and promote relaxation.",
+        "Avoid excessive consumption of caffeine and opt for herbal teas or decaf options.",
+        "Include fiber-rich foods like whole grains, legumes, and vegetables in your diet.",
+        "Limit salt intake to maintain healthy blood pressure levels.",
+        "Practice mindfulness during meals, focusing on enjoying and savoring each bite.",
+        "Engage in activities that stimulate your brain, such as puzzles or learning new skills.",
+        "Take regular breaks from screens and engage in outdoor activities for fresh air and sunlight.",
+        "Surround yourself with positive influences and cultivate a supportive environment."
+    ]
     console.log('Schedule for', Platform.OS);
+    const randomTipIndex = Math.floor(Math.random() * tipArray.length);
+    console.log("randomTipIndex:", randomTipIndex)
+    const randomHealthTip = tipArray[randomTipIndex];
 
     try {
         const permissions = await Notifications.getPermissionsAsync();
@@ -279,7 +315,7 @@ async function scheduleReminder(notificationTime) {
         const id = await Notifications.scheduleNotificationAsync({
             content: {
                 title: 'Daily Health Tip',
-                body: 'Get enough sleep to support your overall well-being.',
+                body: randomHealthTip,
                 sound: true,
                 subtitle: 'Stay Healthy!',
                 color: 'green',
@@ -305,7 +341,7 @@ async function scheduleReminder(notificationTime) {
     }
     catch {
         return false;
-    }
+    }   
 }
 
 async function cancelReminder() {
